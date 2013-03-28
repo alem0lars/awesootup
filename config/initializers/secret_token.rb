a73af3f242b3406029d848801ca619e4fb90a2be
Alessandro Molari <molari.alessandro@gmail.com>
2013-03-28 11:02:51 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Awesootup::Application.config.secret_token = 'c3633efc85143df20233be78d32650aa9cdb336249e81ace7d1918ed3d29d7ece43fd533e2404791b7bb9fce72d15974b7027947ecaa00d65f1dd7db9c5c1fbd'
