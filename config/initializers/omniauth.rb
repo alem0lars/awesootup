0b4dd5df6fc11d751150efa9f8268d61745301f4
Alessandro Molari <molari.alessandro@gmail.com>
2013-07-12 09:21:03 +0200

    authentication fixes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
+# Enable the use of omniauth
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
     provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
@@ -5,3 +6,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     provider :developer
   end
 end
+
+# Use the configured Rails.logger (instead of stdout)
+OmniAuth.config.logger = Rails.logger
